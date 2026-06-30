7d74bfe9b4d2ebb2c06c1b7cac52e146cbaeacaa
madhu3207 <madhusantha03@gmail.com>
2026-06-30 15:34:14 +0530

    Latest

---
+++ Day2/Example10.java
@@ -0,0 +1,11 @@
+package Day2;
+
+public class Example10 {
+    public static void main(String[] args) {//array
+        int[] marks = {80, 90, 100};
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
+
--- Day2/Example9.java
+++ Day2/Example9.java
@@ -1,6 +1,10 @@
 package Day2;
 
-public class Example9 {
-    
-    
+public class Example9 {//string
+    public static void main(String[] args) {
+        String name = "Madhu";
+        System.out.println(name);
+    }
 }
+    
+
